e770af5151dc1eabdcecbc7059fd87dae68e0d84
Fran Pineda <pndafran@gmail.com>
2024-03-14 00:14:10 -0600

    Change schema telephone number now

--- src/models/users.js
+++ src/models/users.js
@@ -1,16 +1,11 @@
 const { Decimal128 } = require('mongodb');
 const { Schema, model } = require('mongoose');
 
-const phoneSchema = new Schema({
-    type: String,
-    match: [/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/, 'Enter a valid phone number']
-});
-
 const UserSchema = new Schema({
     idmentioned: { type: BigInt, default: 4 },    
     names: { type: String, required: true },         
     surnames: { type: String, required: true },        
-    telephone: { type: [ phoneSchema ], index: true, unique: true },
+    telephone: { type: String, match: [/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/, 'Enter a valid phone number']},
     petition: { type: String },      
     baptized: { type: Boolean },        
     status_: { type: Boolean },          
