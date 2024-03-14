147515534768515d2210f0a01d6ebd1ba44d01ab
Fran Pineda <pndafran@gmail.com>
2024-03-13 18:29:21 -0600

    Change enviroment for connected mongo db

--- src/database.js
+++ src/database.js
@@ -2,7 +2,11 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URI, {
+const PART_URI = "mongodb+srv://FranTest:p%2540ssword@craesdb.hg9l9bp"
+
+const DB_URI = `${PART_URI}${process.env.MONGO_URI}`
+
+mongoose.connect(DB_URI, { //process.env.DB_URI
     useNewUrlParser: true,
     dbName: 'craesdb'    
 }).then(db => console.log(`DB is connected ${db}`))
